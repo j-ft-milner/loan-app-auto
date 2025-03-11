7fcef3dcdb977ec89872be117b3f2c07c4efc703
Josh Milner <j.milner@corecomtechacademy.co.uk>
2025-03-11 11:19:37 +0000

    finised application submit test

--- src/main/java/pageObjects/ApplicationPage.java
+++ src/main/java/pageObjects/ApplicationPage.java
@@ -1,6 +1,63 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 public class ApplicationPage extends BasePage {
 
-    
+    public ApplicationPage(){
+
+    }
+
+    public String getErrorMessage(){
+        WebElement errorMessage = driver.findElement(By.cssSelector("div.mt-4.alert.alert-danger"));
+        return errorMessage.getText();
+    }
+
+    public String getResultText(){
+        WebElement successMessage = driver.findElement(By.cssSelector("p.lead"));
+
+        return successMessage.getText();
+    }
+
+    public void populateApplicationForm(String firstName, String lastName, String age, String addressLine1, String addressLine2,
+                                        String addressLine3, String addressLine4, String postcode, String email, String loanAmount){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("formFirstName")));
+
+        WebElement firstNameBox = driver.findElement(By.id("formFirstName"));
+        firstNameBox.sendKeys(firstName);
+
+        WebElement lastNameBox = driver.findElement(By.id("formLastName"));
+        lastNameBox.sendKeys(lastName);
+
+        WebElement ageBox = driver.findElement(By.id("formAge"));
+        ageBox.sendKeys(age);
+
+        WebElement addressLine1Box = driver.findElement(By.id("formAddressLine1"));
+        addressLine1Box.sendKeys(addressLine1);
+
+        WebElement addressLine2Box = driver.findElement(By.id("formAddressLine2"));
+        addressLine2Box.sendKeys(addressLine2);
+
+        WebElement addressLine3Box = driver.findElement(By.id("formAddressLine3"));
+        addressLine3Box.sendKeys(addressLine3);
+
+        WebElement addressLine4Box = driver.findElement(By.id("formAddressLine4"));
+        addressLine4Box.sendKeys(addressLine4);
+
+        WebElement postcodeBox = driver.findElement(By.id("formPostcode"));
+        postcodeBox.sendKeys(postcode);
+
+        WebElement emailBox = driver.findElement(By.id("formEmail"));
+        emailBox.sendKeys(email);
+
+        WebElement loanAmountBox = driver.findElement(By.id("formLoanAmount"));
+        loanAmountBox.sendKeys(loanAmount);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button.btn.btn-primary"));
+        actions.moveToElement(submitButton).click().perform();
+
+    }
 }
--- src/test/java/Tests/TestNewLoanApplication.java
+++ src/test/java/Tests/TestNewLoanApplication.java
@@ -1,7 +1,9 @@
 package Tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pageObjects.ApplicationPage;
 import pageObjects.HomePage;
 import pageObjects.LogInPage;
 
@@ -20,6 +22,20 @@ public class TestNewLoanApplication {
         homePage.clickLoginButton();
         login.login();
         homePage.clickApply();
+        ApplicationPage applicationPage = new ApplicationPage();
+        applicationPage.populateApplicationForm(
+                "bob",
+                "bill",
+                "30",
+                "123",
+                "4",
+                "City",
+                "",
+                "12345",
+                "test@test",
+                "10000");
+
+        Assertions.assertEquals("The initial application has been successful.", applicationPage.getResultText());
     }
 
 
