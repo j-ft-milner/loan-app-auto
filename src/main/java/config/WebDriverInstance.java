28c1df648b8a53967c3a44cb643d6b370753c688
Josh Milner <j.milner@corecomtechacademy.co.uk>
2025-03-14 11:49:09 +0000

    headless

--- src/main/java/config/WebDriverInstance.java
+++ src/main/java/config/WebDriverInstance.java
@@ -3,6 +3,7 @@ package config;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -11,9 +12,13 @@ public class WebDriverInstance {
     private static WebDriver driver;
 
     public static WebDriver getInstance(){
-        if (driver == null){
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         }
         return driver;
