85380bb5b8458744b468a4d680ab36dec5143dd1
Josh Milner <j.milner@corecomtechacademy.co.uk>
2025-03-17 09:49:27 +0000

    fix

--- src/test/java/CucumberTests.java
+++ src/test/java/CucumberTests.java
@@ -1,9 +1,12 @@
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.*;
+import org.junit.runner.RunWith;
 
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+
 public class CucumberTests {
 
 }
